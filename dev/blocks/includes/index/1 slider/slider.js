3d9006882c1e615f61797b7b16a4e5742f8572a8
horsik1991 <horsik1991@gmail.com>
2022-05-15 17:22:42 +0500

    Изменена структура слайдера. Вместо картинки теперь фон. Добавлена адаптивность

--- dev/blocks/includes/index/1 slider/slider.js
+++ dev/blocks/includes/index/1 slider/slider.js
@@ -6,16 +6,16 @@ $(document).ready(function(){
     checker.click(function(){
         if ($(this).attr('id')==='slide1') {
             mainPageColor.css('background-color','#849d8f');
-            mainPageImage.attr('src','static/images/content/1.png');
+            mainPageImage.css('background-image','url(static/images/content/1.png)');
             mainPageTextIceCream.text('Крем-брюле и пломбир с малиновым джемом')
         } else
         if ($(this).attr('id')==='slide2'){
             mainPageColor.css('background-color','#8996a6');
-            mainPageImage.attr('src','static/images/content/2.png');
+            mainPageImage.css('background-image','url(static/images/content/2.png)');
             mainPageTextIceCream.text('Шоколадный пломбир и лимонный сорбет')
         } else {
             mainPageColor.css('background-color','#9d8b84');
-            mainPageImage.attr('src','static/images/content/3.png');
+            mainPageImage.css('background-image','url(static/images/content/3.png)');
             mainPageTextIceCream.text('Пломбир с помадкой и клубничный щербет')
         }
     });
